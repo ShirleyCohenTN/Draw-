9476f9d8bab07414885724f51acbda351babadf4
nitssanZ <bhmi102@gmail.com>
2022-02-08 18:33:41 +0200

    added socket.io

--- draw/src/App.js
+++ draw/src/App.js
@@ -1,26 +1,25 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./DrawPage";
-import Login from "./Login"
-import SignUp  from "./SignUp";
-
+import Login from "./Login";
+import SignUp from "./SignUp";
+import { SocketContext, socket } from "./socket";
 
 function App() {
   return (
     // <div>
     //     <HomePage/>
     // </div>
-
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp />} />
-
-      </Routes>
-    </Router>
-
+    <SocketContext.Provider value={socket}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </Router>
+    </SocketContext.Provider>
   );
-};
+}
 
 export default App;
--- draw/src/DrawPage.js
+++ draw/src/DrawPage.js
@@ -1,86 +1,108 @@
 import React from "react";
-import { useEffect, useRef, useState } from "react";
-import Menu from "./Menu"
+import { useEffect, useRef, useState, useContext } from "react";
+import Menu from "./Menu";
 import "./DrawPage.css";
 
-
-
-// const HomePage = () => {
-
-//     return <div>hidd</div>
-// }
-
-// export default HomePage;
+import { SocketContext } from "./socket";
 
 function DrawPage() {
-    const canvasRef = useRef(null);
-    const ctxRef = useRef(null);
-    const [isDrawing, setIsDrawing] = useState(false);
-    const [lineWidth, setLineWidth] = useState(5);
-    const [lineColor, setLineColor] = useState("black");
-    const [lineOpacity, setLineOpacity] = useState(0.1);
-    
-    // Initialization when the component
-    // mounts for the first time
-    useEffect(() => {
-      const canvas = canvasRef.current;
-      const ctx = canvas.getContext("2d");
-      ctx.lineCap = "round";
-      ctx.lineJoin = "round";
-      ctx.globalAlpha = lineOpacity;
-      ctx.strokeStyle = lineColor;
-      ctx.lineWidth = lineWidth;
-      ctxRef.current = ctx;
-    }, [lineColor, lineOpacity, lineWidth]);
-    
-    // Function for starting the drawing
-    const startDrawing = (e) => {
+  const socket = useContext(SocketContext);
+  const canvasRef = useRef(null);
+  const ctxRef = useRef(null);
+  const [isDrawing, setIsDrawing] = useState(false);
+  const [lineWidth, setLineWidth] = useState(5);
+  const [lineColor, setLineColor] = useState("black");
+  const [lineOpacity, setLineOpacity] = useState(0.1);
+
+  // Initialization when the component
+  // mounts for the first time
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    ctx.lineCap = "round";
+    ctx.lineJoin = "round";
+    ctx.globalAlpha = lineOpacity;
+    ctx.strokeStyle = lineColor;
+    ctx.lineWidth = lineWidth;
+    ctxRef.current = ctx;
+
+    //
+    //socket logic start
+    socket.on("connect", () => {
+      console.log(`connectionID: ${socket.id}`);
+    });
+
+    socket.on("receive-draw", (drawXY) => {
+      ctxRef.current.lineTo(drawXY.X, drawXY.Y);
+      ctxRef.current.stroke();
+      console.log(drawXY.X, drawXY.Y);
+    });
+
+    socket.on("receive-start", (drawXY) => {
       ctxRef.current.beginPath();
-      ctxRef.current.moveTo(
-        e.nativeEvent.offsetX, 
-        e.nativeEvent.offsetY
-      );
+      ctxRef.current.moveTo(drawXY.X, drawXY.Y);
       setIsDrawing(true);
-    };
-    
-    // Function for ending the drawing
-    const endDrawing = () => {
+    });
+
+    socket.on("receive-end", () => {
       ctxRef.current.closePath();
       setIsDrawing(false);
-    };
-    
-    const draw = (e) => {
-      if (!isDrawing) {
-        return;
-      }
-      ctxRef.current.lineTo(
-        e.nativeEvent.offsetX, 
-        e.nativeEvent.offsetY
-      );
-        
-      ctxRef.current.stroke();
-    };
-    
-    return (
-      <div className="App">
-        <h1>Draw!</h1>
-        <div className="draw-area">
-          <Menu
-            setLineColor={setLineColor}
-            setLineWidth={setLineWidth}
-            setLineOpacity={setLineOpacity}
-          />
-          <canvas
-            onMouseDown={startDrawing}
-            onMouseUp={endDrawing}
-            onMouseMove={draw}
-            ref={canvasRef}
-            width={`1280px`}
-            height={`620px`}
-          />
-        </div>
+      console.log("END");
+    });
+    //socket logic END
+    //
+  }, [lineColor, lineOpacity, lineWidth]);
+
+  // Function for starting the drawing
+  const startDrawing = (e) => {
+    ctxRef.current.beginPath();
+    ctxRef.current.moveTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+    setIsDrawing(true);
+    let drawXY = { X: e.nativeEvent.offsetX, Y: e.nativeEvent.offsetY };
+    socket.emit("send-start", drawXY);
+  };
+
+  // Function for ending the drawing
+  const endDrawing = () => {
+    ctxRef.current.closePath();
+    setIsDrawing(false);
+    socket.emit("send-end");
+  };
+
+  const draw = (e) => {
+    if (!isDrawing) {
+      return;
+    }
+    ctxRef.current.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+    let drawXY = { X: e.nativeEvent.offsetX, Y: e.nativeEvent.offsetY };
+    socket.emit("send-draw", drawXY);
+    ctxRef.current.stroke();
+  };
+
+  const t = () => {
+    console.log("yaaaaaaaa");
+  };
+
+  return (
+    <div className="App">
+      <h1>Draw!</h1>
+      <div className="draw-area">
+        <Menu
+          setLineColor={setLineColor}
+          setLineWidth={setLineWidth}
+          setLineOpacity={setLineOpacity}
+        />
+        <canvas
+          onMouseDown={startDrawing}
+          onMouseUp={endDrawing}
+          onMouseMove={draw}
+          ref={canvasRef}
+          width={`1280px`}
+          height={`620px`}
+        />
       </div>
-    );
-  }
-    
-  export default DrawPage;
+    </div>
+  );
+}
+
+export default DrawPage;
