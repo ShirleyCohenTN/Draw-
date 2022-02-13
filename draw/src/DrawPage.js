ba54d0b04adeb58fa446f48dfa2f275027fce049
nitssanZ <bhmi102@gmail.com>
2022-02-13 17:38:24 +0200

    fixed bug 'drawing together simultaneously' added chat capability

--- draw/src/DrawPage.js
+++ draw/src/DrawPage.js
@@ -3,6 +3,8 @@ import { useEffect, useRef, useState, useContext } from "react";
 import Menu from "./Menu";
 import "./DrawPage.css";
 import { SocketContext } from "./socket";
+import ChatWrite from "./chat/ChatWrite";
+import ChatBox from "./chat/Chat";
 
 function DrawPage() {
   const socket = useContext(SocketContext);
@@ -11,7 +13,7 @@ function DrawPage() {
   const [isDrawing, setIsDrawing] = useState(false);
   const [lineWidth, setLineWidth] = useState(5);
   const [lineColor, setLineColor] = useState("black");
-  const [lineOpacity, setLineOpacity] = useState(0.1);
+  //const [lineOpacity, setLineOpacity] = useState(0.1);
 
   // Initialization when the component
   // mounts for the first time
@@ -20,42 +22,51 @@ function DrawPage() {
     const ctx = canvas.getContext("2d");
     ctx.lineCap = "round";
     ctx.lineJoin = "round";
-    ctx.globalAlpha = lineOpacity;
+    //ctx.globalAlpha = lineOpacity;
     ctx.strokeStyle = lineColor;
     ctx.lineWidth = lineWidth;
     ctxRef.current = ctx;
-
+    let senders = {};
     //
     //socket logic start
     socket.on("connect", () => {
       console.log(`connectionID: ${socket.id}`);
     });
 
-    socket.on("receive-draw", (drawXY) => {
+    socket.on("receive-draw", (drawXY, senderID) => {
+      console.log("draw:", drawXY, senderID);
+      let drawFrom = senders[senderID];
+      ctxRef.current.beginPath();
+      ctxRef.current.moveTo(drawFrom.X, drawFrom.Y);
       ctxRef.current.lineTo(drawXY.X, drawXY.Y);
       ctxRef.current.stroke();
+      ctxRef.current.closePath();
       console.log(drawXY.X, drawXY.Y);
+      senders[senderID] = drawXY;
     });
 
-    socket.on("receive-start", (drawXY) => {
-      ctxRef.current.beginPath();
-      ctxRef.current.moveTo(drawXY.X, drawXY.Y);
+    socket.on("receive-start", (drawXY, senderID) => {
+      console.log("start", drawXY, senderID);
+      senders[senderID] = drawXY;
+
+      //ctxRef.current.beginPath();
+      //ctxRef.current.moveTo(drawXY.X, drawXY.Y);
       setIsDrawing(true);
     });
 
     socket.on("receive-end", () => {
-      ctxRef.current.closePath();
+      //ctxRef.current.closePath();
       setIsDrawing(false);
       console.log("END");
     });
     //socket logic END
     //
-  }, [lineColor, lineOpacity, lineWidth]);
+  }, [lineColor, lineWidth]);
 
   // Function for starting the drawing
   const startDrawing = (e) => {
-    ctxRef.current.beginPath();
-    ctxRef.current.moveTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+    // ctxRef.current.beginPath();
+    // ctxRef.current.moveTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
     setIsDrawing(true);
     let drawXY = { X: e.nativeEvent.offsetX, Y: e.nativeEvent.offsetY };
     socket.emit("send-start", drawXY);
@@ -65,7 +76,7 @@ function DrawPage() {
     if (!isDrawing) {
       return;
     }
-    ctxRef.current.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
+    //ctxRef.current.lineTo(e.nativeEvent.offsetX, e.nativeEvent.offsetY);
     let drawXY = { X: e.nativeEvent.offsetX, Y: e.nativeEvent.offsetY };
     socket.emit("send-draw", drawXY);
     ctxRef.current.stroke();
@@ -85,7 +96,7 @@ function DrawPage() {
         <Menu
           setLineColor={setLineColor}
           setLineWidth={setLineWidth}
-          setLineOpacity={setLineOpacity}
+          //setLineOpacity={setLineOpacity}
         />
         <canvas
           onMouseDown={startDrawing}
@@ -96,6 +107,7 @@ function DrawPage() {
           height={`620px`}
         />
       </div>
+      <ChatBox />
     </div>
   );
 }
--- draw/src/socket.js
+++ draw/src/socket.js
@@ -5,22 +5,5 @@ import { io } from "socket.io-client";
 //socket.io logic
 export const socket = io("http://192.168.1.9:3030");
 export const SocketContext = React.createContext();
-const socketPack = () => {
-  socket.on("connect", () => {
-    console.log(`connection ${socket.id}`);
-  });
-
-  socket.on("receive-draw", (drawXY) => {
-    console.log(drawXY.X, drawXY.Y);
-  });
-
-  socket.on("receive-start", (drawXY) => {
-    console.log("start at: ", drawXY.X, drawXY.Y);
-  });
-
-  socket.on("receive-end", () => {
-    console.log("END");
-  });
-};
 //socket.io logic end
 //
