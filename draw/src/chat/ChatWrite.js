ef58e389769b490b61dbb56f9cc60647ad3be0f2
nitssanZ <bhmi102@gmail.com>
2022-02-13 17:43:14 +0200

    chat ability added

---
+++ draw/src/chat/ChatWrite.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { useEffect, useRef, useState, useContext } from "react";
+import { SocketContext } from "../socket";
+
+function ChatWrite() {
+  const socket = useContext(SocketContext);
+  const [chatMassage, setChatMassage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit("send-chat", chatMassage);
+  };
+
+  const handleChange = (e) => {
+    setChatMassage(e.target.value);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>
+          massage:
+          <input type="text" value={chatMassage} onChange={handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
+
+export default ChatWrite;
