4cc31c31a3872c07cb0755c061077a4ef5719626
Shirley Cohen <shilychanxd@hotmail.com>
2022-03-03 17:36:23 +0200

    added "delete" option to "myCanvases.js"

--- draw/src/pages/myCanvases/MyCanvases.js
+++ draw/src/pages/myCanvases/MyCanvases.js
@@ -1,93 +1,117 @@
-import React , { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./css/MyCanvases.css";
-
+// import delete from '../../images/delete.png';
 
 var url = "http://localhost:50434/api/canvases";
 
-
-
-
 function MyCanvases() {
-    const [canvases, setCanvases] = useState([]);
- 
-    var img = new Image();
+  const [canvases, setCanvases] = useState([]);
+   const [canvasesAfterDelete, setCanvasesAfterDelete] = useState([]);
 
-    useEffect(() => {
-      console.log("im here");
+  var img = new Image();
 
-      // getAllCanvasesByUser();
-      
-      getAllCanvases();
+  useEffect(() => {
+    console.log("im here");
 
+    // getAllCanvasesByUser();
 
-    }, [])
-    
+    getAllCanvases();
+  }, []);
 
+  const getAllCanvases = () => {
+    fetch(url, {
+      method: "GET", // 'GET', 'POST', 'PUT', 'DELETE', etc.
+      headers: new Headers({
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      }),
+    }) // Call the fetch function passing the url of the API as a parameter
+      .then((resp) => {
+        if (resp.status === 200) {
+          return resp.json();
+        } else return "could not get all the Canvases!";
+      }) // Transform the data into json
+      .then((data) => {
+        if (!data.toString().includes("could not get all the canvases!")) {
+          setCanvases(data);
+          console.log(data);
+        } else {
+          console.log("couldnt get");
+        }
+      })
+      .catch(function (err) {
+        alert(err);
+      });
+  };
 
-    const getAllCanvases = () => {
-        fetch(url,
-            {
-                method: 'GET', // 'GET', 'POST', 'PUT', 'DELETE', etc.
-                headers: new Headers({
-                    'Content-Type': 'application/json',
-                    'Accept': 'application/json'
-                }),
-            }) // Call the fetch function passing the url of the API as a parameter
-            .then((resp) => {
-                if (resp.status === 200) {
-                    return resp.json();
-                }
-                else
-                    return "could not get all the Canvases!";
-            }
-            ) // Transform the data into json
-            .then((data) => {
-                if (!data.toString().includes("could not get all the canvases!")) {
-                    setCanvases(data);
-                    console.log(data);
-                }
-                else {
-                    console.log('couldnt get');
-                }
-            })
-            .catch(function (err) {
-                alert(err);
-            });
-    }
+  const btnDelete = (Canvas_ID) => {
+    console.log("we're in the delete button");
+    console.log("the canvas id is = ", Canvas_ID);
 
-  
-  
-  
-  
-  
-  
-  
-  
-  
-    return (
-    <div>
-      <h2 style={{ textAlign: "center" }}>Canvases Gallery List</h2>
-  
-<div>
-    {canvases.map((item, index) => {
-        return(
-            <div key={index}>
+     fetch(url + `?canvas_id=${Canvas_ID}`,
+     {
+         method: 'DELETE', // 'GET', 'POST', 'PUT', 'DELETE', etc.
+         headers: new Headers({
+             // 'Content-Type': 'application/json',
+             'Accept': 'application/json'
+         }),
+     }) // Call the fetch function passing the url of the API as a parameter
+     .then((resp) => {
+         // console.log(resp);
+         if (resp.status === 200) {
+             console.log(200);
+             let newCanvases = canvases.filter(c => c.Canvas_ID !== Canvas_ID);
+             setCanvases(newCanvases);
 
-                {/*  BASE64 אני לא יודעת איך להמיר את זה לתמונה ולא*/}
-                {/* <img src = {`data:image/png;base64,${item.Canvas_Path}`}/> */}
-                <img src = {item.Canvas_Path}/>
-                <img src = "https://facebook.github.io/react-native/docs/assets/favicon.png" />
-                {/* <Image ></Image> */}
-      {/* img.onload = () => {
-        ctxRef.current.drawImage(img, 0, 0);
-      }; */}
+         }
+         else if (resp.status === 400) {
+             console.log("BadRequest");
+         }
+         else {
+             console.log("NotFound");
+         }
+     }
+     ) // Transform the data into json
+     .catch(function (err) {
+         alert(err);
+     });
+  };
 
+  return (
+    <div style={{ backgroundColor: "gray" }}>
+      <h2 style={{ textAlign: "center" }}>My Canvases</h2>
 
-             </div>
-        )
-    })}
-</div>
+      <div>
+        {canvases.map((item, index) => {
+          return (
+            <div key={index} className="div-style">
+              <img className="canvas-img" src={item.Canvas_Path} />
 
+              <div style={{ display: "block" }}>
+                <button className="button-style">
+                  Edit
+                  {/* <img style={{width:"30%", display:"inline-block"}} src={require('../../images/delete.png')} /> */}
+                </button>
+
+                <button
+                  className="button-style"
+                  onClick={() => btnDelete(item.Canvas_ID)}
+                >
+                  Delete
+                  {/* <img style={{width:"30%", display:"inline-block"}} src={require('../../images/delete.png')} /> */}
+                </button>
+
+                <button className="button-style">
+                  Duplicate
+                  {/* <img style={{width:"30%", display:"inline-block"}} src={require('../../images/delete.png')} /> */}
+                </button>
+              </div>
+
+              {/* <img src = "https://facebook.github.io/react-native/docs/assets/favicon.png" /> */}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
